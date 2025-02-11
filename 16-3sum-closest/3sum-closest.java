eb6039c8ef73dd20c9b003f0e79212d135d0dc18
Adesh Kumar <adeshak0707@gmail.com>
2025-02-11 21:51:36 +0530

    Time: 14 ms (76.20%) | Memory: 43 MB (94.62%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+      //first we need to sort the nums
+      Arrays.sort(nums); 
+      int closetSum=nums[0]+nums[1]+nums[2];
+      //now fix first element and find the ohter two element
+      
+      for(int i=0;i<nums.length - 2;i++){
+        //Find others two elements using Two Sum approch
+        int left=i+1;
+        int right=nums.length -1;
+        while(left<right){
+            int sum=nums[i]+nums[left]+nums[right];
+            if(sum==target){
+                return target;
+            }
+            if(Math.abs(sum - target) < Math.abs(closetSum - target)){
+                closetSum=sum;
+            }
+            if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }   
+      }
+      return closetSum;
+    }
+}
