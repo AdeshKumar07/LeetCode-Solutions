4a789d8b69083a1fda87066ed4f0f227b3fa1982
Adesh Kumar <adeshak0707@gmail.com>
2025-02-11 21:52:03 +0530

    Time: 15 ms (42.51%) | Memory: 43.6 MB (24.19%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -5,7 +5,7 @@ class Solution {
       int closetSum=nums[0]+nums[1]+nums[2];
       //now fix first element and find the ohter two element
       
-      for(int i=0;i<nums.length - 2;i++){
+      for(int i=0;i<nums.length - 1;i++){
         //Find others two elements using Two Sum approch
         int left=i+1;
         int right=nums.length -1;
