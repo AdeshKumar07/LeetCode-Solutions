0abe6b40541a10459d53f6e503d961e956fc9b20
Adesh Kumar <adeshak0707@gmail.com>
2026-09-02 15:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (16.73%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        // reason because as we know given number in range of 1 to n means odd is also presnent so 
+        // we can say if we subtract even-odd then we alway get a odd number , so its always true 
        return true;
         
     }
