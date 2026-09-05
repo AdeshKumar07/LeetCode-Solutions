496df91433718d862db07ce0c67f55c1a122bd2f
Adesh Kumar <adeshak0707@gmail.com>
2026-09-06 00:24:26 +0530

    Time: 2 ms (99.36%) | Memory: 47.2 MB (23.79%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,7 +6,8 @@ class Solution {
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
             }
-            map.put(nums[i], i);
+
+          map.put(nums[i], i);
         }
         return new int[0];
     }
